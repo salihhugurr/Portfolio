a0f60a95de553f168838713f47a5df11a3e81dd3
Salih <salihugur46@gmail.com>
2022-04-08 00:08:28 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {About,Footer,Header,Skills,Testimonial,Work} from './container'
+import {Navbar} from './components';
+import {Helmet} from 'react-helmet';
+import './App.scss'
+const App = () => {
+  return (
+    <div className='app'>
+      <Helmet>
+        <meta charSet='utf-8'/>
+        <title>Salih</title>
+      </Helmet>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        {/* <Testimonial /> */}
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {FaFacebookF,FaGithub,FaInstagram, FaTwitter} from 'react-icons/fa'
+
+
+const SocialMedia = () => {
+    return(
+        <div className="app__social">
+            <div>
+                <a className="app__flex" href="https://www.instagram.com/ugurrsalihh/">
+                    <FaInstagram />
+                </a>
+            </div>
+            <div>
+                <a className="app__flex" href="https://github.com/salihhugurr">
+                    <FaGithub />
+                </a>
+            </div>
+            <div>
+                <a className="app__flex" href="https://twitter.com/salihhugurr">
+                    <FaTwitter/>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default SocialMedia;
