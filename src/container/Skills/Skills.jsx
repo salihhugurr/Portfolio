4315cb367b44ecf0eb2896170de73b6beed58ddf
Salih <salihugur46@gmail.com>
2025-01-24 17:20:42 +0300

    updates

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -36,44 +36,73 @@ const Skills = () => {
               className="app__skills-item app__flex"
               key={skill.name}
             >
-              <div
-                className="app__flex"
-                style={{ backgroundColor: "#edf2f8" }}
-              >
-                <img src={urlFor(skill.icon)} alt={skill.name} style={{borderRadius:50}}/>
+              <div className="app__flex" style={{ backgroundColor: "#edf2f8" }}>
+                <img
+                  src={urlFor(skill.icon)}
+                  alt={skill.name}
+                  style={{ borderRadius: 50 }}
+                />
               </div>
               <p className="p-text">{skill.name}</p>
             </motion.div>
           ))}
         </motion.div>
         <div className="app__skills-exp">
-          <motion.div className="app__skills-exp-item" key={"2022"}>
+          <motion.div className="app__skills-exp-item" key={"2024"}>
             <div className="app__skills-exp-year">
-              <p className="bold-text">{"01.06.2022 - Current"}</p>
+              <p className="bold-text">{"04.08.2024 - Current"}</p>
             </div>
             <motion.div className="app__skills-exp-works">
-
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                    className="app__skills-exp-work"
-                    data-tip
-                    data-for={"Frontend Developer"}
-                    key={"Frontend Developer"}
-                  >
-                    <h4 className="bold-text">{"Frontend Developer"}</h4>
-                    <p className="p-text">{"Smartiks Teknoloji"}</p>
-                  </motion.div>
-                  <ReactTooltip
-                    id={"Frontend Developer"}
-                    effect="solid"
-                    arrowColor="#fff"
-                    className="skills-tooltip"
-                  >
-                    Developing applications for customers using React & React Native & NodeJS 
-                    Integrating existing and newly developed applications. 
-                    Converting Figma designed products into web and mobile applications
-                  </ReactTooltip>
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="app__skills-exp-work"
+                data-tip
+                data-for={"Frontend Developer"}
+                key={"Frontend Developer"}
+              >
+                <h4 className="bold-text">{"React Native Developer"}</h4>
+                <p className="p-text">{"OTO Global Inc."}</p>
+              </motion.div>
+              <ReactTooltip
+                id={"Frontend Developer"}
+                effect="solid"
+                arrowColor="#fff"
+                className="skills-tooltip"
+              >
+                Developing applications for OTO using React & React Native
+                Integrating existing and newly developed applications.
+                Converting Figma designed products into mobile applications
+              </ReactTooltip>
+            </motion.div>
+          </motion.div>
+          <motion.div className="app__skills-exp-item" key={"2022"}>
+            <div className="app__skills-exp-year">
+              <p className="bold-text">{"01.06.2022 - 01.08.2024"}</p>
+            </div>
+            <motion.div className="app__skills-exp-works">
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="app__skills-exp-work"
+                data-tip
+                data-for={"Frontend Developer"}
+                key={"Frontend Developer"}
+              >
+                <h4 className="bold-text">{"Frontend Developer"}</h4>
+                <p className="p-text">{"Smartiks Teknoloji"}</p>
+              </motion.div>
+              <ReactTooltip
+                id={"Frontend Developer"}
+                effect="solid"
+                arrowColor="#fff"
+                className="skills-tooltip"
+              >
+                Developing applications for customers using React & React Native
+                & NodeJS Integrating existing and newly developed applications.
+                Converting Figma designed products into web and mobile
+                applications
+              </ReactTooltip>
             </motion.div>
           </motion.div>
 
@@ -82,21 +111,19 @@ const Skills = () => {
               <p className="bold-text">{"01.08.2021 - 01.06.2022"}</p>
             </div>
             <motion.div className="app__skills-exp-works">
-
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                    className="app__skills-exp-work"
-                    data-tip
-                    data-for={"HEY"}
-                    key={"HEY"}
-                  >
-                    <h4 className="bold-text">{"React Native Developer"}</h4>
-                    <p className="p-text">{"HEY TEKNOLOJİ"}</p>
-                  </motion.div>
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="app__skills-exp-work"
+                data-tip
+                data-for={"HEY"}
+                key={"HEY"}
+              >
+                <h4 className="bold-text">{"React Native Developer"}</h4>
+                <p className="p-text">{"HEY TEKNOLOJİ"}</p>
+              </motion.div>
             </motion.div>
           </motion.div>
-
         </div>
       </div>
     </>
--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { AiFillEye, AiFillGithub,AiFillSecurityScan } from 'react-icons/ai';
-import { FaGooglePlay,FaAppStore } from 'react-icons/fa';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub, AiFillSecurityScan } from "react-icons/ai";
+import { FaGooglePlay, FaAppStore } from "react-icons/fa";
+import { motion } from "framer-motion";
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
-import './Work.scss';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+import "./Work.scss";
 
 const Work = () => {
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
-  const [activeFilter, setActiveFilter] = useState('All');
+  const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   useEffect(() => {
     const query = '*[_type == "works"]';
 
     client.fetch(query).then((data) => {
-      console.log("data",data);
-      setWorks(data);
-      setFilterWork(data);
+      const sortedData = data.sort(
+        (a, b) => new Date(b._createdAt) - new Date(a._createdAt)
+      );
+
+      setWorks(sortedData);
+      setFilterWork(sortedData);
     });
   }, []);
 
@@ -30,7 +33,7 @@ const Work = () => {
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if (item === 'All') {
+      if (item === "All") {
         setFilterWork(works);
       } else {
         setFilterWork(works.filter((work) => work.tags.includes(item)));
@@ -40,18 +43,24 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My <span>Works</span></h2>
+      <h2 className="head-text">
+        My <span>Works</span>
+      </h2>
 
       <div className="app__work-filter">
-        {['UI/UX','Mobile App','Web App','TypeScript','AI','All'].map((item, index) => (
-          <div
-            key={index}
-            onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
-          >
-            {item}
-          </div>
-        ))}
+        {["UI/UX", "Mobile App", "Web App", "TypeScript", "AI", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? "item-active" : ""
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
       </div>
 
       <motion.div
@@ -61,75 +70,74 @@ const Work = () => {
       >
         {filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
-            <div
-              className="app__work-img app__flex"
-            >
+            <div className="app__work-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
                 className="app__work-hover app__flex"
               >
-                {
-                  !work.codeLink &&
+                {!work.codeLink && (
                   <>
-                  <a href={work.appStore} target="_blank" rel="noreferrer">
-
-                    <motion.div
-                      whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.90] }}
-                      transition={{ duration: 0.25 }}
-                      className="app__flex"
-                    >
-                      <FaAppStore />
-                    </motion.div>
-                  </a>
-                  <a href={work.playStore} target="_blank" rel="noreferrer">
-
-                    <motion.div
-                      whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.90] }}
-                      transition={{ duration: 0.25 }}
-                      className="app__flex"
-                    >
-                      <FaGooglePlay />
-                    </motion.div>
-                  </a>
+                    <a href={work.appStore} target="_blank" rel="noreferrer">
+                      <motion.div
+                        whileInView={{ scale: [0, 1] }}
+                        whileHover={{ scale: [1, 0.9] }}
+                        transition={{ duration: 0.25 }}
+                        className="app__flex"
+                      >
+                        <FaAppStore />
+                      </motion.div>
+                    </a>
+                    <a href={work.playStore} target="_blank" rel="noreferrer">
+                      <motion.div
+                        whileInView={{ scale: [0, 1] }}
+                        whileHover={{ scale: [1, 0.9] }}
+                        transition={{ duration: 0.25 }}
+                        className="app__flex"
+                      >
+                        <FaGooglePlay />
+                      </motion.div>
+                    </a>
                   </>
-                }
-                {
-                  work.projectLink &&
+                )}
+                {work.projectLink && (
                   <a href={work.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillSecurityScan />
-                  </motion.div>
-                </a>
-                }
-                {
-                  work.codeLink &&
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillSecurityScan />
+                    </motion.div>
+                  </a>
+                )}
+                {work.codeLink && (
                   <a href={work.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
-                }
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                )}
               </motion.div>
             </div>
 
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
 
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
@@ -143,7 +151,7 @@ const Work = () => {
 };
 
 export default AppWrap(
-  MotionWrap(Work, 'app__works'),
-  'work',
-  'app__primarybg',
+  MotionWrap(Work, "app__works"),
+  "work",
+  "app__primarybg"
 );
